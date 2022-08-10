6b205e2b6cc5ea1a509dfeeb059c64bea8dbd288
Mauro Crispim <70303414+mcrispim@users.noreply.github.com>
2022-08-10 13:08:55 -0300

    Cria Extensions.kt na pasta core

---
+++ app/src/main/java/com/example/diogithubportfolio/core/Extensions.kt
@@ -0,0 +1,47 @@
+package com.example.diogithubportfolio.core
+
+import android.app.Activity
+import android.app.AlertDialog
+import android.content.Context
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.ProgressBar
+import com.example.diogithubportfolio.R
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
+import com.google.android.material.textfield.TextInputLayout
+
+var TextInputLayout.text: String
+    get() = editText?.text?.toString() ?: ""
+    set(value) {
+        editText?.setText(value)
+    }
+
+fun View.hideSoftKeyboard() {
+    val imm = context.getSystemService(Activity.INPUT_METHOD_SERVICE) as InputMethodManager
+    imm.hideSoftInputFromWindow(windowToken, 0)
+}
+
+//
+// Obs.: No exemplo original o autor usou AlertDialog e não androidx.appcompat.app.AlertDialog.
+//
+fun Context.createDialog(block: MaterialAlertDialogBuilder.() -> Unit = {}): androidx.appcompat.app.AlertDialog {
+    val builder = MaterialAlertDialogBuilder(this)
+    builder.setPositiveButton(android.R.string.ok, null)
+    block(builder)
+    return builder.create()
+}
+
+//
+// Obs.: No exemplo original o autor usou AlertDialog e não androidx.appcompat.app.AlertDialog.
+//
+fun Context.createProgressDialog(): androidx.appcompat.app.AlertDialog {
+    return createDialog {
+        val padding = this@createProgressDialog.resources.getDimensionPixelOffset(R.dimen.my_layout_padding)
+        val progressBar = ProgressBar(this@createProgressDialog)
+        progressBar.setPadding(padding, padding, padding, padding)
+        setView(progressBar)
+
+        setPositiveButton(null, null)
+        setCancelable(false)
+    }
+}
