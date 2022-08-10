b6d7c799e6aaf3fb615beac6a7baf72d9e14e0d4
Mauro Crispim <70303414+mcrispim@users.noreply.github.com>
2022-08-10 14:02:55 -0300

    Cria App.kt

--- app/src/main/java/com/example/diogithubportfolio/ui/MainActivity.kt
+++ app/src/main/java/com/example/diogithubportfolio/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.diogithubportfolio
+package com.example.diogithubportfolio.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.diogithubportfolio.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
