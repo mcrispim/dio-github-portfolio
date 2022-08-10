cc2e9dcf5c715f5795ea2e6bdeae28d4ce0e782c
Mauro Crispim <70303414+mcrispim@users.noreply.github.com>
2022-08-10 13:24:48 -0300

    Cria UseCase.kt na pasta core

--- app/src/main/java/com/example/diogithubportfolio/core/Extensions.kt
+++ app/src/main/java/com/example/diogithubportfolio/core/Extensions.kt
@@ -1,7 +1,6 @@
 package com.example.diogithubportfolio.core
 
 import android.app.Activity
-import android.app.AlertDialog
 import android.content.Context
 import android.view.View
 import android.view.inputmethod.InputMethodManager
